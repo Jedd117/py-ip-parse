8a5f8fc83ac93fea85c540c30c7aaf2c520378b2
Jeff Baxter <jeffrey.baxter@outlook.com>
2021-05-26 16:14:03 -0400

    Adjusted so IP addresses are sorted.

--- sort-ip.py
+++ sort-ip.py
@@ -2,8 +2,10 @@
 from os import write
 import sys
 import re
+import ipaddress
 
-# Resource for this base script: https://www.geeksforgeeks.org/extract-ip-address-from-file-using-python/
+# Resources for this script: https://www.geeksforgeeks.org/extract-ip-address-from-file-using-python/
+# https://stackoverflow.com/questions/6545023/how-to-sort-ip-addresses-stored-in-dictionary-in-python
 
 # variable for file to read
 file = sys.argv[1]
@@ -28,12 +30,16 @@ for line in fstring:
 # file_name will name the dump file as the name of the imported file + '-report.txt'
 file_name = file.split('.txt')[0] + "-report.txt"
 
+# Sorting IP addresses in order
+sortedkey = sorted(lst, key = ipaddress.IPv4Address)
+
 # Creating new file to dump output
 # w+ creates a file if one is not in use. 
 # Per instructions, writing to stdout
 sys.stdout = open(file_name, 'w+')
 
 with sys.stdout as new_file:
-    for line in lst:
+    print("--------------")
+    for line in sortedkey:
         print(line)
 sys.stdout.close()
