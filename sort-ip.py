111602a2a2619a5884aa41e6127e3b002e0048ec
Jeff Baxter <jeffrey.baxter@outlook.com>
2021-05-26 15:22:45 -0400

    Updated to write as stdout

--- sort-ip.py
+++ sort-ip.py
@@ -7,7 +7,10 @@ import re
 
 # variable for file to read
 file = sys.argv[1]
- 
+
+# Adding stdout
+var = sys.stdout
+
 # opening and reading the file
 with open(file) as fh:
    fstring = fh.readlines()
@@ -20,13 +23,17 @@ lst=[]
  
 # extracting the IP addresses
 for line in fstring:
-   lst.append(pattern.search(line)[0])
+    lst.append(pattern.search(line)[0])
 
 # file_name will name the dump file as the name of the imported file + '-report.txt'
 file_name = file.split('.txt')[0] + "-report.txt"
 
 # Creating new file to dump output
 # w+ creates a file if one is not in use. 
-with open(file_name, 'w+') as new_file:
+# Per instructions, writing to stdout
+sys.stdout = open(file_name, 'w+')
+
+with sys.stdout as new_file:
     for line in lst:
-        new_file.write(line + "\n")
+        print(line)
+sys.stdout.close()
